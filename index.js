47103968bb734869510682a17705a5835386cd10
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2018-09-06 22:58:11 -0700

    new files

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require("passport")
+require("./models/user.js")
+require("./services/passport.js");
+const keys = require('./config/keys')
 
+const app = express();
 
-const red = {
-    hi: "There",
-    hello: "no",
-}
+app.use(
+    cookieSession({
+        maxAge: 4 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get("/", (req, res) => {
-    res.send(red.hello)
-})
+mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
+
+require("./routes/authRoutes.js")(app)
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT)
---
+++ services/passport.js
@@ -0,0 +1,56 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const keys = require("../config/keys.js")
+const mongoose = require("mongoose")
+
+const User = mongoose.model("users")
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+            done(null, user)
+        })
+})
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+       
+       User.findOne({ googleID: profile.id })
+       .then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser)
+            } else {
+                new User({ googleID: profile.id })
+                .save()
+                .then(user => done(null, user))
+            }
+       })
+
+    })
+);
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'passwd'
+  },
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+));
