de7cafbf28e0687d993e4739adff6ca8f287e044
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2018-09-07 21:19:26 -0700

    helper sucks

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,7 @@ const passport = require('passport');
 module.exports = (app) => {
 
     app.get("/auth/google", passport.authenticate("google", {
-        scope: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
-        ]
+        scope: ["profile", "email"]
     })
     )
     
