b59634f102965ed13920c926ab30965bdde5c2c0
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2018-09-08 01:07:55 -0700

    new callback

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,7 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
-        proxy: true
+        callbackURL: "https://cryptic-reef-91374.herokuapp.com/auth/google/callback"
     }, (accessToken, refreshToken, profile, done) => {
        
        User.findOne({ googleID: profile.id })
