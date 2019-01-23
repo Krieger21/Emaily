533bc9e75560640bfa17300c1e017213fa237694
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2019-01-23 13:52:08 -0800

    proxy

--- services/passport.js
+++ services/passport.js
@@ -18,10 +18,10 @@ passport.deserializeUser((id, done) => {
 
 passport.use(
     new GoogleStrategy({
+        proxy: true,
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback",
-        proxy: true
+        callbackURL: "/auth/google/callback"
     }, (accessToken, refreshToken, profile, done) => {
        
        User.findOne({ googleID: profile.id })
