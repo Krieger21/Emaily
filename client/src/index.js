664c2a3c22190a1b8cd252fbf207bae3ccfe84f9
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2019-01-28 23:09:24 -0800

    Added in authentication Reducer

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
 import App from './components/App.js';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
+
+
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
