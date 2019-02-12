f05aac25fb2ee430bcea3dd5d7e33275e62744c9
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2019-02-11 22:47:17 -0800

    Finished billing

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const cookieSession = require('cookie-session')
-const passport = require("passport")
-require("./models/user.js")
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require("passport");
+const keys = require('./config/keys');
+const bodyParser = require('body-parser');
+require("./models/user.js");
 require("./services/passport.js");
-const keys = require('./config/keys')
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 4 * 60 * 60 * 1000,
@@ -21,7 +23,8 @@ mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
 
-require("./routes/authRoutes.js")(app)
+require("./routes/authRoutes.js")(app);
+require("./routes/billingRoutes.js")(app);
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT)
--- models/user.js
+++ models/user.js
@@ -4,7 +4,11 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     googleID: String,
     username: String,
-    password: String
+    password: String,
+    credits: {
+        type: Number,
+        default: 0
+    }
 })
 
 mongoose.model("users", userSchema)
